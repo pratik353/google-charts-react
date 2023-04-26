774e6596cf51c9b7047b8d55811aa94f7f32aad9
Pratik Kamble <Pratik.K@parallel-minds.com>
2023-04-26 14:26:57 +0530

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AreaChart from "./Chart-Types/Area-chart/AreaChart";
+import BarChart from "./Chart-Types/Bar-chart/BarChart";
+import GeoChart from "./Chart-Types/GeoChart/GeoChart";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AreaChart />
+      <BarChart />
+      <GeoChart />
     </div>
   );
 }
